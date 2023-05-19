ab1940ba920ed8c06c4c41ce17e0c29b4d7c1b54
yawn <69970183+yawn-c111@users.noreply.github.com>
2023-05-19 14:18:13 +0900

    feat: add env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-};
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    local: {
+      url: "http://localhost:8545",
+      accounts: [process.env.LOCAL_PRIVATE_KEY as string],
+    },
+  },
+}
 
-export default config;
+export default config
